bee5e1a84616d416e358ec5e612449284ccae821
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-05-18 19:11:00 +0100

    cache mvp

--- cmd/run.go
+++ cmd/run.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-	"sort"
+	"syscall"
+	"time"
 
+	cachePkg "github.com/fornellas/blitzbuild/pkg/cache"
 	cmdPkg "github.com/fornellas/blitzbuild/pkg/cmd"
+	"github.com/fornellas/resonance/log"
 	"github.com/spf13/cobra"
 )
 
@@ -15,33 +17,109 @@ var RunCmd = &cobra.Command{
 	Short: "Run command.",
 	Long:  "TODO",
 	Args:  cobra.MinimumNArgs(1),
-	Run: func(cobraCmd *cobra.Command, args []string) {
+	RunE: func(cobraCmd *cobra.Command, args []string) error {
+		logger := log.MustLogger(cobraCmd.Context())
+
+		cache, err := cachePkg.NewCache[map[string]time.Time]()
+		if err != nil {
+			return err
+		}
+
 		name := args[0]
 		args = args[1:]
-
 		cmdPtraceFile, err := cmdPkg.NewCmdPtraceFile(name, args, nil, "")
 		if err != nil {
-			fmt.Printf("NewCmdPtraceFile: %T: %s\n", err, err)
-			os.Exit(1)
+			return fmt.Errorf("failed to create command: %w", err)
 		}
+		key := cmdPtraceFile.Id()
+
+		logger.Info("Checking if cached")
+		value, err := cache.Get(key)
+		if err != nil {
+			return fmt.Errorf("failed to get from cache: %w", err)
+		}
+
+		shouldRun := false
+		if value != nil {
+			logger.Info("We have in cache, checking if any changes")
+			for path, tim := range value {
+				var stat_t syscall.Stat_t
+				err := syscall.Stat(path, &stat_t)
+				if err != nil {
+					if errno, ok := err.(syscall.Errno); ok {
+						if !(errno == syscall.ENOTDIR || errno == syscall.ENOENT) {
+							return fmt.Errorf("failed to stat file: %w", err)
+						}
+					}
+					if !tim.IsZero() {
+						logger.Info("file exists on cache, but does not exist now", "path", path)
+						shouldRun = true
+						break
+					}
+				} else {
+					mtim := time.Unix(stat_t.Mtim.Sec, stat_t.Mtim.Nsec)
+					ctim := time.Unix(stat_t.Ctim.Sec, stat_t.Ctim.Nsec)
+					if tim.IsZero() {
+						logger.Info("file didn't exist on cache, but existis now", "path", path)
+						shouldRun = true
+						break
+					} else if mtim.After(tim) || ctim.After(tim) {
+						logger.Info("file modified", "path", path)
+						shouldRun = true
+						break
+					}
+				}
+			}
+			if !shouldRun {
+				logger.Info("no file changes", "count", len(value))
+			}
+		} else {
+			logger.Info("Not in cache, running")
+			shouldRun = true
+		}
+
+		if !shouldRun {
+			logger.Info("Success (cached)")
+			return nil
+		}
+		logger.Info("Running")
 
 		ctx := context.Background()
 		fileMap, err := cmdPtraceFile.Run(ctx)
 		if err != nil {
-			fmt.Printf("CmdPtraceFile.Run: %T: %s\n", err, err)
-			os.Exit(1)
+			return fmt.Errorf("command failed: %w", err)
 		}
 
-		files := make([]string, len(fileMap))
-		i := 0
-		for file := range fileMap {
-			files[i] = file
-			i++
+		logger.Info("Stat files for caching")
+		value = map[string]time.Time{}
+		for path := range fileMap {
+			var tim time.Time
+			var stat_t syscall.Stat_t
+			err := syscall.Stat(path, &stat_t)
+			if err != nil {
+				if errno, ok := err.(syscall.Errno); ok {
+					if !(errno == syscall.ENOTDIR || errno == syscall.ENOENT) {
+						return fmt.Errorf("failed to stat: %w (errno %d)", err, errno)
+					}
+				} else {
+					return fmt.Errorf("failed to stat: %w (%T)", err, err)
+				}
+			} else {
+				tim = time.Unix(stat_t.Mtim.Sec, stat_t.Mtim.Nsec)
+				ctim := time.Unix(stat_t.Ctim.Sec, stat_t.Ctim.Nsec)
+				if ctim.After(tim) {
+					tim = ctim
+				}
+			}
+			value[path] = tim
 		}
-		sort.Strings(files)
-		for _, file := range files {
-			fmt.Printf("%s\n", file)
+		if err := cache.Put(key, value); err != nil {
+			return fmt.Errorf("faild to put to cache: %w", err)
 		}
+
+		logger.Info("Success")
+
+		return nil
 	},
 }
 
---
+++ pkg/cache/cache.go
@@ -0,0 +1,106 @@
+package cache
+
+import (
+	"crypto/sha512"
+	"encoding/gob"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// Cache is a cache implementation that persists data to the filesystem.
+type Cache[T any] struct {
+	cacheDir string
+}
+
+// NewPersistentCache creates a new persistent cache in the user's cache directory.
+func NewCache[T any]() (*Cache[T], error) {
+	userCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user cache directory: %w", err)
+	}
+
+	cacheDir := filepath.Join(userCacheDir, "blitzbuild")
+	if err := os.MkdirAll(cacheDir, 0755); err != nil {
+		return nil, fmt.Errorf("failed to create cache directory: %w", err)
+	}
+
+	return &Cache[T]{
+		cacheDir: cacheDir,
+	}, nil
+}
+
+func (c *Cache[T]) getFilePath(key string) string {
+	h := sha512.New()
+	h.Write([]byte(key))
+	hashedKey := hex.EncodeToString(h.Sum(nil))
+	return filepath.Join(c.cacheDir, hashedKey)
+}
+
+// Put saves a value to the cache.
+func (c *Cache[T]) Put(key string, value T) error {
+	filePath := c.getFilePath(key)
+	tempFilePath := filePath + "-temp"
+
+	if _, err := os.Stat(filePath); err == nil {
+		if err := os.Remove(filePath); err != nil {
+			return fmt.Errorf("failed to remove existing cache file: %w", err)
+		}
+	}
+
+	tempFile, err := os.OpenFile(tempFilePath, os.O_RDWR|os.O_TRUNC|os.O_CREATE, os.FileMode(0644))
+	if err != nil {
+		return fmt.Errorf("failed to open temp file: %w", err)
+	}
+
+	encoder := gob.NewEncoder(tempFile)
+	if err = encoder.Encode(value); err != nil {
+		err = fmt.Errorf("failed to encode value: %w", err)
+		if err := os.Remove(tempFilePath); err != nil {
+			err = errors.Join(err, fmt.Errorf("failed to remove temp file: %w", err))
+		}
+		return err
+	}
+
+	if err = tempFile.Close(); err != nil {
+		err = fmt.Errorf("failed to close temp file: %w", err)
+		if err := os.Remove(tempFilePath); err != nil {
+			err = errors.Join(err, fmt.Errorf("failed to remove temp file: %w", err))
+		}
+		return err
+	}
+
+	if err = os.Rename(tempFilePath, filePath); err != nil {
+		err = fmt.Errorf("failed to move temp file: %w", err)
+		if err := os.Remove(tempFilePath); err != nil {
+			err = errors.Join(err, fmt.Errorf("failed to remove temp file: %w", err))
+		}
+		return err
+	}
+
+	return nil
+}
+
+// Get retrieves a value from the cache.
+func (c *Cache[T]) Get(key string) (value T, err error) {
+	filePath := c.getFilePath(key)
+
+	var file *os.File
+	file, err = os.Open(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return value, nil
+		}
+		return value, fmt.Errorf("failed to open cache file: %w", err)
+	}
+	defer func() { err = errors.Join(err, file.Close()) }()
+
+	decoder := gob.NewDecoder(file)
+	if err = decoder.Decode(&value); err != nil {
+		return value, fmt.Errorf("failed to decode cached value: %w", err)
+	}
+
+	return value, nil
+}
